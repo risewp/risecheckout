a7ba1bb8bb9e0064393654b0e58bebab2e852bde
Gilberto Tavares <camaleaun@gmail.com>
2025-02-14 22:50:36 -0300

    First commit

---
+++ risecheckout.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Rise Checkout
+ * Description: Enhanced WooCommerce checkout with a seamless single-page multi-step with tracking for abandoned carts.
+ * Version: 1.0.0
+ * Author: RiseWP
+ */
